e1c76239b935d6207535732fcb1b68a919695541
Flawake <60014996+Flawake@users.noreply.github.com>
2025-04-16 23:28:03 +0200

    fix(store_items_amounts_change)

--- Assets/Core/Store/StoreItemObject.cs
+++ Assets/Core/Store/StoreItemObject.cs
@@ -6,4 +6,14 @@ public class StoreItemObject : ScriptableObject
     public ItemObject itemObject;
     public int itemPriceFishCoins;
     public int itemPriceFishBucks;
+    
+    public StoreItemObject Clone()
+    {
+        StoreItemObject clone = Instantiate(this);
+        if (itemObject != null)
+        {
+            clone.itemObject = Instantiate(itemObject);
+        }
+        return clone;
+    }
 }
