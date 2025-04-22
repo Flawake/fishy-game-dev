c348b98128d07a6902fd9bfbc6f50dbd8bc2997d
Flawake <60014996+Flawake@users.noreply.github.com>
2025-04-22 16:32:35 +0200

    fix(updated env variables)

--- Assets/Utils/EnvConfig.cs
+++ Assets/Utils/EnvConfig.cs
@@ -1,25 +1,26 @@
 using System;
+using UnityEngine;
 
 // "https://fishydatabase.djoleden.nl/", "http://127.0.0.1:8080/"
 public static class EnvConfig
 {
     public static string DatabaseAccessServer = "";
     public static string DatabaseAccessToken = "";
-    public static ushort Port = 0;
-    public static ushort ClientPort = 0;
+    public static ushort Port = 24468;
+    public static ushort ClientPort = 24469;
     
     public static void LoadEnv()
     {
         #if UNITY_EDITOR
             DatabaseAccessServer = "https://fishydatabase.djoleden.nl/";
             DatabaseAccessToken = "t";
-            Port = 25568;
-            ClientPort = 25569;
-        #else
+            Port = 24468;
+            ClientPort = 24469;
+        #elif !UNITY_WEBGL
             DatabaseAccessServer = Environment.GetEnvironmentVariable("DATABASE_ACCESS_SERVER");
             DatabaseAccessToken = Environment.GetEnvironmentVariable("DATABASE_ACCESS_TOKEN");
-            Port = Environment.GetEnvironmentVariable("PORT");
-            ClientPort = Environment.GetEnvironmentVariable("CLIENT_PORT");
+            Port = ushort.Parse(Environment.GetEnvironmentVariable("SERVER_PORT"));
+            ClientPort = ushort.Parse(Environment.GetEnvironmentVariable("CLIENT_PORT"));
         #endif
     }
 }
