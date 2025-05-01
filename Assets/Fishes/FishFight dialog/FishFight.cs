e5e043e74c717d009f746a42abb4dcf7d9071453
michiel2005 <60014996+Flawake@users.noreply.github.com>
2025-05-01 13:12:16 +0200

    Fix(fishing minigame and anti-cheat issue)

--- Assets/Fishes/FishFight dialog/FishFight.cs
+++ Assets/Fishes/FishFight dialog/FishFight.cs
@@ -1,3 +1,5 @@
+using Microsoft.Cci;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
@@ -11,11 +13,14 @@ public class FishFight : MonoBehaviour
         impossible
     }
 
+    private float startedFightTime = 0;
+
     private bool right_pressed;
     private bool left_pressed;
 
     [SerializeField] GameObject fishFightDialog;
     [SerializeField] GameObject fightSlider;
+    [SerializeField] RectTransform fightSliderTransform;
     [SerializeField] Slider progressBar;
     [SerializeField] RectTransform redRight;
     [SerializeField] RectTransform redLeft;
@@ -30,13 +35,13 @@ public class FishFight : MonoBehaviour
 
     float rodPower;
     float fishPower;
-    float sail;
+    float totalFishPull;
     float sailRandom;
-    int idle;
+    float idle;
     float progress;
     int rarity;
     int gameSize;
-    int realGameSize;
+    float realGameSize;
     float relativePos;
     [SerializeField]
     float sensitivity = 0.5f;
@@ -44,10 +49,10 @@ public class FishFight : MonoBehaviour
 
     int minFishingTimeSeconds;
 
-    void RandomGameSize(int minFishingTime)
+    int RandomGameSize(int minFishingTime)
     {
         int random = Random.Range(100, 200);
-        gameSize = random * minFishingTime;
+        return random * minFishingTime;
     }
 
     void FightDone(FishingManager.EndFishingReason reason)
@@ -63,14 +68,33 @@ public class FishFight : MonoBehaviour
         }
     }
 
+    public float CalculateFishPower(int fishSize) {
+        //Returns number between 0.97 and 1.06
+        float randomFishPowerMultiplier = (11f + Random.Range(0f, 1f) - 0.33f) / 11f;
+        //Add 50 as a offset, so that the fight power of the fish and rod do not become too small
+        return 50 + (fishSize * randomFishPowerMultiplier);
+    }
+
+    public float CalculateRodPower(int rodStrength, float fishPower)
+    {
+        if(rodStrength / 2 > fishPower)
+        {
+            return 50 + fishPower * 3;
+        }
+        return 50 + rodStrength;
+    }
+
     public void StartFight(CurrentFish currentFish, int minFishingTime)
     {
         playerData = GetComponentInParent<PlayerData>();
+        Debug.Log($"FishLength: {currentFish.length}");
+        Debug.Log($"RodStrength: {playerData.GetSelectedRod().strength}");
 
-        RandomGameSize(minFishingTime);
+        gameSize = RandomGameSize(minFishingTime);
 
-        sail = 0;
+        totalFishPull = 0;
         sailRandom = Random.Range(0f, 1f);
+        sensitivity = Random.Range(0f, 1f);
         idle = 0;
         progress = 0;
         realGameSize = 0;
@@ -78,11 +102,8 @@ public class FishFight : MonoBehaviour
 
         currentFishOnHook = currentFish;
 
-        //Returns number between 0.97 and 1.06
-        float randomFishPowerMultiplier = (11f + Random.Range(0f, 1f) - 0.33f) / 11f;
-        //Add 50 as a offset, so that the fight power of the fish and rod do not become too small
-        fishPower = 50 + (currentFishOnHook.length * randomFishPowerMultiplier);
-        rodPower = 50 + playerData.GetSelectedRod().strength;
+        fishPower = CalculateFishPower(currentFishOnHook.length);
+        rodPower = CalculateRodPower(playerData.GetSelectedRod().strength, fishPower);
 
         rarity = FishEnumConfig.RatityToInt(currentFishOnHook.rarity);
         redLeft.sizeDelta = new Vector2((1.0f / 8.0f * rarity + 1.0f / 8.0f) / 2.0f * fishFightArea.rect.width, 50);
@@ -91,7 +112,9 @@ public class FishFight : MonoBehaviour
         progressBar.value = progressBar.minValue;
         minFishingTimeSeconds = minFishingTime;
         
+        startedFightTime = Time.time;
         initialized = true;
+        StartCoroutine(startMiniGame());
     }
 
     public void EndFight()
@@ -151,8 +174,14 @@ public class FishFight : MonoBehaviour
 
         progressBar.value = progress / 100;
 
-        if (progress / 100 > progressBar.maxValue)
+        // Make sure that the progress bar is filled AND the time is enough, accumelated float errors and frame timing can mess up the totalfishingtime and stop a few ms too early, which the anti-cheat than catches.
+        if (progress / 100 > progressBar.maxValue && Time.time - startedFightTime > minFishingTimeSeconds)
         {
+            if(Time.time - startedFightTime < minFishingTimeSeconds)
+            {
+                Debug.LogWarning($"That was a lil to early ({minFishingTimeSeconds - (Time.time - startedFightTime)})");
+                Debug.LogWarning($"minFishingTimeSeconds: {minFishingTimeSeconds}, startedFightTime: {startedFightTime}, Time: {Time.time}");
+            }
             FightDone(FishingManager.EndFishingReason.caughtFish);
         }
         //minValue is 0, but we start with 0 so that would make us instantly fail. Check if it is less than 0 instead.
@@ -162,22 +191,46 @@ public class FishFight : MonoBehaviour
         }
     }
 
-    void ReelInFish()
+    private IEnumerator startMiniGame() {
+        var nextFrameTime = Time.realtimeSinceStartup + 0.035f;
+
+        while (initialized)
+        {
+            processMinigameTick();
+
+            // Calculate how long to wait
+            var currentTime = Time.realtimeSinceStartup;
+            var waitTime = nextFrameTime - currentTime;
+
+            if (waitTime > 0)
+            {
+                yield return new WaitForSecondsRealtime(waitTime);
+            }
+            else
+            {
+                // If we're running behind, yield null to at least let other coroutines run
+                yield return null;
+            }
+
+            nextFrameTime += 0.035f;
+        }
+    }
+
+    private void processMinigameTick()
     {
-        float newSail;
-        float sail1 = Random.Range(0f, 1f) - 0.5f;
-        float sail2 = Random.Range(0f, 1f) - 0.5f;
+        float fishPull; 
+        float fishPullOption1 = Random.Range(-0.5f, 0.5f);
+        float fishPullOption2 = Random.Range(-0.5f, 0.5f);
         float power;
+
         realGameSize++;
 
-        if(Mathf.Abs(sail1) > Mathf.Abs(sail2))
-        {
-            newSail = sail1;
-        }
-        else
-        {
-            newSail = sail2;
-        }
+        // Choose the stronger fish pull
+        fishPull = Mathf.Abs(fishPullOption1) > Mathf.Abs(fishPullOption2) 
+            ? fishPullOption1 
+            : fishPullOption2;
+        
+        // Reset or increase idle counter based on player input
         if(left_pressed || right_pressed)
         {
             idle = 0;
@@ -186,24 +239,31 @@ public class FishFight : MonoBehaviour
         {
             idle++;
         }
+        
+        // Scale fish pull by progress
         if(progress < 40)
         {
-            newSail = newSail * progress / 40;
+            fishPull *= progress / 40;
         }
-        if(idle > 40)
+        // Scale fish pull by ídleness
+        if (idle > 40)
         {
-            newSail = newSail * idle / 40;
+            fishPull *= idle / 40;
         }
-        if (Random.Range(0f, 1f) < 0.5f && (newSail < 0 && sail > 0 || newSail > 0 && sail < 0))
+        
+        // 50% change to flip the direction if the current pull vector is opposite to the total pull vector
+        if (Random.Range(0f, 1f) < 0.5f && (fishPull < 0 && totalFishPull > 0) || (fishPull > 0 && totalFishPull < 0))
         {
-            newSail = -newSail;
+            fishPull = -fishPull;
         }
+        
+        // Determine power based on fish vs rod power, and randomly scale totalFishPull
         if(fishPower > rodPower * 1.2f)
         {
             power = fishPower / rodPower * 2.4f;
             if((left_pressed || right_pressed) && Random.Range(0f, 1f) < 0.12f + rarity / 60)
             {
-                sail = -2.2f * sail;
+                totalFishPull *= -2.2f;
             } 
         }
         else if(fishPower > rodPower)
@@ -211,7 +271,7 @@ public class FishFight : MonoBehaviour
             power = fishPower / rodPower * 1.8f;
             if ((left_pressed || right_pressed) && Random.Range(0f, 1f) < 0.08f + rarity / 60)
             {
-                sail = -1.6f * sail;
+                totalFishPull *= -1.6f;
             }
         }
         else
@@ -219,38 +279,46 @@ public class FishFight : MonoBehaviour
             power = fishPower / rodPower * 0.8f;
             if ((left_pressed || right_pressed) && Random.Range(0f, 1f) < 0.04f + rarity / 70)
             {
-                sail = -0.4f * sail;
+                totalFishPull *= -0.4f;
             }
         }
+        
+        // Minimum power
         if(power < 0.1f)
         {
             power = 0.1f;
         }
+        
+        // Modify power based on rarity and game progress
         power *= 0.7f + rarity / 3;
-        power += realGameSize / gameSize / 3;
-        //
-        power *= 3f;
-        sail += newSail * power;
-        sail = sail * (7 + sailRandom - 0.5f) / 7;
+        power += realGameSize / (float)gameSize / 3;
+        
+        //apply current fishPull to the totalFishPull
+        totalFishPull += fishPull * power;
+        totalFishPull *= (7 + sailRandom - 0.5f) / 7;
+
+        // Handle player input
         if (left_pressed && !right_pressed)
         {
-            relativePos -= 1f - rarity / 20f + sensitivity;
-            sail *= 0.8f;
-            if(sail > 0 && Random.Range(0f, 1f) < 0.08f)
+            relativePos -= 0.4f - rarity / 40f + sensitivity / 4;
+            totalFishPull *= 0.8f;
+            if(totalFishPull > 0 && Random.Range(0f, 1f) < 0.08f)
             {
-                sail = -0.8f * sail;
+                totalFishPull *= -0.8f;
             }
         }
         else if (!left_pressed && right_pressed)
         {
-            relativePos += 1f - rarity / 20f + sensitivity;
-            sail *= 0.8f;
-            if (sail < 0 && Random.Range(0f, 1f) < 0.08f)
+            relativePos += 0.4f - rarity / 40f + sensitivity / 4;
+            totalFishPull *= 0.8f;
+            if (totalFishPull < 0 && Random.Range(0f, 1f) < 0.08f)
             {
-                sail = -0.8f * sail;
+                totalFishPull *= -0.8f;
             }
         }
-        relativePos += sail;
+        
+        // Finally, update the minigame position
+        relativePos += totalFishPull * 4;
     }
 
     private void Start()
@@ -262,17 +330,18 @@ public class FishFight : MonoBehaviour
     private void FixedUpdate()
     {
         if (!initialized)
-            return;
-        ReelInFish();
-
-        if(relativePos + fightSlider.GetComponent<RectTransform>().rect.width / 2 > fishFightArea.rect.width / 2)
         {
-            relativePos = (fishFightArea.rect.width / 2) + (-fightSlider.GetComponent<RectTransform>().rect.width / 2);
+            return;
+        }
+
+        if(relativePos + fightSliderTransform.rect.width / 2 > fishFightArea.rect.width / 2)
+        {
+            relativePos = (fishFightArea.rect.width / 2) + (-fightSliderTransform.rect.width / 2);
             FightDone(FishingManager.EndFishingReason.lostFish);
         }
-        else if (relativePos - fightSlider.GetComponent<RectTransform>().rect.width / 2 < -fishFightArea.rect.width / 2)
+        else if (relativePos - fightSliderTransform.rect.width / 2 < -fishFightArea.rect.width / 2)
         {
-            relativePos = (-fishFightArea.rect.width / 2) + (fightSlider.GetComponent<RectTransform>().rect.width / 2);
+            relativePos = (-fishFightArea.rect.width / 2) + (fightSliderTransform.rect.width / 2);
             FightDone(FishingManager.EndFishingReason.lostFish);
         }
     }
