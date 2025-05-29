6056811dd9753eacada5c8691a13f0b172e87cd5
Flawake <60014996+Flawake@users.noreply.github.com>
2025-05-29 13:20:05 +0200

    Feat(Added date and time ranges for when fishes can be caught)

--- Assets/Fishes/Scripts/FishConfig/FishConfiguration.cs
+++ Assets/Fishes/Scripts/FishConfig/FishConfiguration.cs
@@ -1,16 +1,92 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class TimeRange
+[System.Serializable]
+public struct SerializableTime
 {
-    public TimeSpan BeginTime;
-    public TimeSpan EndTime;
+    [Range(0, 23)] public int hours;
+    [Range(0, 59)] public int minutes;
+    
+    public TimeSpan ToTimeSpan() => new TimeSpan(hours, minutes, 0);
 }
 
+[System.Serializable]
+public struct SerializableDate
+{
+    [Range(1, 12)] public int month;
+    [Range(1, 31)] public int day;
+    
+    public DateTime ToDateTime(int year = 2000)
+    {
+        // Clamp the day to the max days in the month to prevent exceptions
+        int daysInMonth = DateTime.DaysInMonth(year, month);
+        int safeDay = Mathf.Clamp(day, 1, daysInMonth);
+        return new DateTime(year, month, safeDay);
+    }
+}
+
+[System.Serializable]
+public class TimeRange
+{
+    public SerializableTime BeginTime;
+    public SerializableTime EndTime;
+
+    public bool TimeRangeContainsTime(int hour, int minute)
+    {
+        return TimeRangeContainsTime(
+            new SerializableTime()
+            {
+                minutes = minute,
+                hours = hour,
+            }
+        );
+    }
+    
+    public bool TimeRangeContainsTime(SerializableTime time)
+    {
+        var start = BeginTime.ToTimeSpan();
+        var end = EndTime.ToTimeSpan();
+        var check = time.ToTimeSpan();
+
+        if (start <= end)
+        {
+            return check >= start && check <= end;
+        }
+        return check >= start || check <= end;
+    }
+}
+
+[System.Serializable]
 public class DateRange
 {
-    public TimeSpan BeginDate;
-    public TimeSpan EndDate;
+    public SerializableDate BeginDate;
+    public SerializableDate EndDate;
+    
+    public bool DateRangeContainsDate(int month, int day)
+    {
+        return DateRangeContainsDate(new SerializableDate { month = month, day = day });
+    }
+
+    public bool DateRangeContainsDate(SerializableDate date)
+    {
+        var dummyYear = 2000;
+
+        DateTime start = BeginDate.ToDateTime(dummyYear);
+        DateTime end = EndDate.ToDateTime(dummyYear);
+        DateTime check = date.ToDateTime(dummyYear);
+
+        if (start <= end)
+        {
+            // Normal range: e.g. Mar 15 - Jun 01
+            return check >= start && check <= end;
+        }
+        else
+        {
+            // Wrap-around range: e.g. Nov 15 - Feb 10
+            return check >= start || check <= end;
+        }
+    }
 }
 
 [CreateAssetMenu(fileName = "FishConfig", menuName = "Fishes/config", order = 1), System.Serializable]
@@ -35,8 +111,8 @@ public class FishConfiguration : ScriptableObject
     [Space(20)]
     //1 is normal, 0.1 is 10 times as rare.
     public float rarityFactor;  
-    public TimeRange timeRange;
-    public DateRange dateRange;
+    public List<TimeRange> timeRanges = new List<TimeRange>();
+    public List<DateRange> dateRanges = new List<DateRange>();
     [Space(20)]
     public int breedSuccessRate;
     public int breedPrice;
