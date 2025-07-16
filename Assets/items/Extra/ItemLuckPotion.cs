f0d649393d310733699ccfc8b3189c7b3a7018e9
Flawake <60014996+Flawake@users.noreply.github.com>
2025-07-16 18:07:14 +0200

    Added some special items

---
+++ Assets/items/Extra/ItemLuckPotion.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public enum LuckPotionType
+{
+    Rare = 0,
+    Epic = 1,
+}
+
+[CreateAssetMenu(fileName = "LuckPotion", menuName = "Items/LuckPotion", order = 2)]
+public class ItemLuckPotion: ItemObject
+{
+    public int amount;
+    public float duration;
+    public LuckPotionType luckType;
+    
+    public string AsString()
+    {
+        switch (luckType)
+        {
+            case LuckPotionType.Rare:
+                return "Rare luck potion";
+            case LuckPotionType.Epic:
+                return "Epic luck potion";
+            default:
+                Debug.LogError("Unknown luck potion type");
+                return "Unknown luck potion type";
+        };
+    }
+}
