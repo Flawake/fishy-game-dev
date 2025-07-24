2beb64fc86f3c2891f3997d30533c4b1d760f3c9
Flawake <60014996+Flawake@users.noreply.github.com>
2025-07-24 07:40:47 +0200

    Small project cleanup

--- Assets/Core/UncaughtFishInfo.cs
+++ Assets/Core/UncaughtFishInfo.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class UncaughtFishInfo : MonoBehaviour
 {
+    //TODO: implement the fishinfo ui for fishes that have not yet been caught
     public void ShowFishInfo(int fishID)
     {
 
