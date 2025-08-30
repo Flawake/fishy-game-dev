85c89fd5c51b25f56fd0d337d1b0a7855798a972
Flawake <60014996+Flawake@users.noreply.github.com>
2025-08-30 22:25:45 +0200

    Fixed buying item issue and deletedunused files

--- Assets/Utils/EnvConfig.cs
+++ Assets/Utils/EnvConfig.cs
@@ -2,25 +2,26 @@ using System;
 using UnityEngine;
 
 // "https://fishydatabase.djoleden.nl/", "http://127.0.0.1:8000/"
+
 public static class EnvConfig
 {
     public static string DatabaseAccessServer = "";
     public static string DatabaseAccessToken = "";
-    public static ushort Port = 24468;
-    public static ushort ClientPort = 24469;
-    
+    public static ushort Port = 24469;
+    public static ushort ClientPort = 443;
+
     public static void LoadEnv()
     {
-        #if UNITY_EDITOR
-            DatabaseAccessServer = "http://127.0.0.1:8000/";
-            DatabaseAccessToken = "t";
-            Port = 24468;
-            ClientPort = 24469;
-        #elif !UNITY_WEBGL
+#if UNITY_EDITOR
+        DatabaseAccessServer = "http://127.0.0.1:8000/";
+        DatabaseAccessToken = "t";
+        Port = 24468;
+        ClientPort = 24469;
+#elif !UNITY_WEBGL
             DatabaseAccessServer = Environment.GetEnvironmentVariable("DATABASE_ACCESS_SERVER");
             DatabaseAccessToken = Environment.GetEnvironmentVariable("DATABASE_ACCESS_TOKEN");
             Port = ushort.Parse(Environment.GetEnvironmentVariable("SERVER_PORT"));
             ClientPort = ushort.Parse(Environment.GetEnvironmentVariable("CLIENT_PORT"));
-        #endif
+#endif
     }
 }
