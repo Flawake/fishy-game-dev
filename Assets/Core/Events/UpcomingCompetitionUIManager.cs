ebc23dddef8cf91a945c93504e2a5c33c3507583
Julius Pleunes <dutchforums@gmail.com>
2026-03-09 22:45:43 +0100

    feedback: remove debugging UUID display field from UpcomingCompetitionUIManager

--- Assets/Core/Events/UpcomingCompetitionUIManager.cs
+++ Assets/Core/Events/UpcomingCompetitionUIManager.cs
@@ -12,19 +12,12 @@ namespace GlobalCompetitionSystem
         [SerializeField] TMP_Text CountdownText;
         [SerializeField] TMP_Text DescriptionText;
         [SerializeField] Image CompetitionIcon;
-        [SerializeField] TMP_Text CompetitionUuidText; // For debugging
 
         public void SetUpcomingCompetition(Competition competition)
         {
             _competition = competition;
             DescriptionText.text = competition.CompetitionState.AsString();
             CompetitionIcon.sprite = competition.CompetitionState.Icon();
-            
-            // Display competition UUID for debugging
-            if (CompetitionUuidText != null)
-            {
-                CompetitionUuidText.text = $"ID: {competition.CompetitionId}";
-            }
         }
 
         private void Update()
